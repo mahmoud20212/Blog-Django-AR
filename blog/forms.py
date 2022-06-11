5b145029576615e8f35a84cdaca8bb4fe7273dd6
mahmoud20212 <occee123@gmail.com>
2022-06-11 17:46:27 +0300

    latest update

--- blog/forms.py
+++ blog/forms.py
@@ -7,7 +7,7 @@ class NewComment(forms.ModelForm):
         fields = ['name', 'email', 'body']
 
 class PostCreateForm(forms.ModelForm):
-    title = forms.CharField(label='عنوان التدوينة')
+    title = forms.CharField(label='عنوان التدوينة', help_text='العنوان يجب ألا يزيد عن 100 حرف')
     content = forms.CharField(label='نص التدوينة', widget=forms.Textarea())
     class Meta:
         model = Post
